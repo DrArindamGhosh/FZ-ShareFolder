b211e9269b533f98e36437dc50753940dfd1c80e
Dr Arindam Ghosh <60941729+DrArindamGhosh@users.noreply.github.com>
2026-04-16 10:50:14 +0100

    Add files via upload

---
+++ Benchmark_Model/Code/2 - Balance_Integrated_Tracks/Berlin/balance_v0_3_Berlin.py
@@ -0,0 +1,213 @@
+import win32com.client
+from steer_control_v3_4_Berlin import calculate_steering_control
+from speed_control_v2_3_Berlin import calculateTorqueControl
+from model_traj_test import plotTraj
+from model_traj_test import plot_e_phi_Values
+from model_traj_test import plot_e_l_Values
+from model_traj_test import plot_balance_Values
+from model_traj_test import plot_Angles
+from model_traj_test import plot_Torque
+from model_traj_test import plot_TargetValues
+from model_traj_test import plot_longThrustValues
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+def main():
+    balance = win32com.client.Dispatch("{5C7A18B0-6AF5-456D-9CFB-F6F651FD31B5}")
+ 
+    print("F9 requested")
+ 
+    nFail = 0
+    localDatabaseHandle = 0
+    localSetupRecord = 0
+ 
+    assert balance is not None, "Balance object is null"
+    balance.compat_SetFailValue(-1)
+ 
+    localDatabaseHandle = balance.OpenLocalDatabase()
+    assert localDatabaseHandle != 0, "Failed to open local database"
+ 
+    dbHandle = balance.OpenEDBConnection("HHDev", "HHDev", "https://hhdm-api.hh-dev.com")
+
+    localSetupRecord = balance.OpenSetupWarehouse(dbHandle, "SetupNameIsID", "4889ecf8-0320-472c-a520-d9ab97f64cc2")
+ 
+    carHandle = balance.OpenCarFromSetup(localSetupRecord)
+
+    # Initial Velocity in x, y and z directions
+    balance.DynamicsConvertCar_Vinit(carHandle, 0, 0, 0)
+    # balance.DynamicsConvertCar_Vinit(carHandle, -100, 100, 0)
+
+    iterations = 1000000
+
+    steering_input = 0
+    MDriveRL = 0 
+    MDriveRR = 0
+    MBrakeFL = 0
+    MBrakeFR = 0
+    MBrakeRL = 0
+    MBrakeRR = 0
+
+    MDriveRL_array = []
+    MDriveRR_array = []
+    MBrakeFL_array = []
+    MBrakeFR_array = []
+    MBrakeRL_array = [] 
+    MBrakeRR_array = []
+    tVelocity_array = []
+    acc_array = []
+    slipThrust_array = []
+    oldThrust_array = []
+
+    x_positions = []
+    y_positions = []
+    yawAngles = []
+    velocity_X = []
+    velocity_Y = []
+
+    e_phi_values = []
+    refLine_orientations = []
+    saturated_e_l_values = [[] for _ in range(8)]
+
+    lever_coordinates = []
+    ideal_coordinates = []
+
+    steering_inputs = []
+    refDistances = []
+    tVCs = []
+
+    errors = [[] for _ in range(8)]
+    previous_targetVelocity = None
+ 
+    for _ in range(iterations):
+        
+        # DynamicsSimExtDrive2() simulates one timestep, where "carHandle" is the setup, the second parameter is steering input, the four "i"s are mechanical torque
+        # # to each of the Front Left, Front Right, Rear Left, Rear Right tyres, and the last four parameters represent braking torque for the Front Left, Front Right, Rear Left, Rear Right tyres
+        # balance.DynamicsSimExtDrive2(carHandle, steering_input, 0, 0, 50, 50, 5, 5, 5, 5)
+        balance.DynamicsSimExtDrive2(carHandle, steering_input, 0, 0, MDriveRL, MDriveRR, MBrakeFL, MBrakeFR, MBrakeRL, MBrakeRR)
+        
+
+        simTime = balance.ReadChannelScalar(carHandle, "Chassis", "", "Sim Time")
+        # print(f"Time Elapsed: {simTime}\n")
+
+
+        # Chassis Position
+
+        x, y, z = balance.ReadChannelVector(carHandle, "Chassis", "", "Position")
+        print(f"Iteration {_+1}:") 
+        print(f"Positions: X Output = {x}")
+        print(f"           Y Output = {y}\n")
+        x_positions.append(x)
+        y_positions.append(y)
+
+        # Yaw Rates
+
+        yawAngle = balance.ReadChannelScalar(carHandle, "Chassis", "", "Vehicle Yaw")
+        yawAngle = yawAngle + np.pi
+        
+        if yawAngle < np.pi/2:
+            yawAngle = yawAngle + 2*np.pi
+            print(f"                 Yaw Angle = {yawAngle}\n")       
+        
+        if yawAngles and yawAngles[-1] > 7:
+            if yawAngle < 2*np.pi: 
+                yawAngle = yawAngle + 2*np.pi
+            print(f"                 Yaw Angle = {yawAngle}\n")
+
+        yawAngles.append(yawAngle)
+
+        # Velocities
+        Vx, Vy, Vz = balance.ReadChannelVector(carHandle, "Chassis", "", "Vehicle Velocity")
+
+        Vx = abs(Vx)
+        Vy = abs(Vy)
+        print(f"Velocities:      X Output = {Vx}")
+        print(f"                 Y Output = {Vy}\n")
+        velocity_X.append(Vx)
+        velocity_Y.append(Vy)
+
+        # Wheel Velocities
+        rearLHSWheelVelocity = balance.ReadChannelScalar(carHandle, "Tyre", "Rear LHS", "Omega")
+        rearRHSWheelVelocity = balance.ReadChannelScalar(carHandle, "Tyre", "Rear RHS", "Omega")
+        # print(f"                 Rear LHS Wheel Velocity = {rearLHSWheelVelocity}")
+        # print(f"                 Rear RHS Wheel Velocity = {rearRHSWheelVelocity}\n")
+
+        # Slip Ratios
+        frontLHSWheelSlipRatio = balance.ReadChannelScalar(carHandle, "Tyre", "Front LHS", "Slip Ratio")
+        frontRHSWheelSlipRatio = balance.ReadChannelScalar(carHandle, "Tyre", "Front RHS", "Slip Ratio")
+        rearLHSWheelSlipRatio = balance.ReadChannelScalar(carHandle, "Tyre", "Rear LHS", "Slip Ratio")
+        rearRHSWheelSlipRatio = balance.ReadChannelScalar(carHandle, "Tyre", "Rear RHS", "Slip Ratio")
+        # print(f"                 Front LHS Wheel Slip Ratio = {frontLHSWheelSlipRatio}")
+        # print(f"                 Front RHS Wheel Slip Ratio = {frontRHSWheelSlipRatio}")
+        # print(f"                 Rear LHS Wheel Slip Ratio = {rearLHSWheelSlipRatio}")
+        # print(f"                 Rear RHS Wheel Slip Ratio = {rearRHSWheelSlipRatio}\n")
+
+        ###########################################################################################################################################################
+        # Steering Control
+        steering_inputs.append(steering_input)
+        steering_input, e_phi, saturated_e_l, levercoordsplot, idealcoordsplot, error_values, refLine_orientation  = calculate_steering_control(Vx, Vy, yawAngle, x, y)
+        
+
+        
+        e_phi_values.append(e_phi)
+        for i in range(8):
+            saturated_e_l_values[i].append(saturated_e_l[i])
+        lever_coordinates.append(levercoordsplot)
+        ideal_coordinates.append(idealcoordsplot)
+        refLine_orientations.append(refLine_orientation)
+
+        ###########################################################################################################################################################
+        # Speed Control
+        MDriveRL, MDriveRR, MBrakeFL, MBrakeFR, MBrakeRL, MBrakeRR, targetVelocity, longAcceleration, slipThrust, oldLongThrust = calculateTorqueControl(Vx, 
+                                                                                                                                                            Vy, 
+                                                                                                                                                            yawAngle, 
+                                                                                                                                                            x, 
+                                                                                                                                                            y, 
+                                                                                                                                                            rearLHSWheelVelocity, 
+                                                                                                                                                            rearRHSWheelVelocity, 
+                                                                                                                                                            frontLHSWheelSlipRatio, 
+                                                                                                                                                            frontRHSWheelSlipRatio, 
+                                                                                                                                                            rearLHSWheelSlipRatio,
+                                                                                                                                                            rearRHSWheelSlipRatio,
+                                                                                                                                                            steering_input)
+
+        MDriveRL_array.append(MDriveRL)
+        MDriveRR_array.append(MDriveRR)
+        MBrakeFL_array.append(MBrakeFL)
+        MBrakeFR_array.append(MBrakeFR)
+        MBrakeRL_array.append(MBrakeRL)
+        MBrakeRR_array.append(MBrakeRR)
+        tVelocity_array.append(targetVelocity)
+        if previous_targetVelocity is not None and abs(targetVelocity - previous_targetVelocity) > 20:
+            print(f"Breaking loop at iteration {_+1} due to Target Velocity difference > 20")
+            break
+        
+        previous_targetVelocity = targetVelocity
+        acc_array.append(longAcceleration)
+        slipThrust_array.append(slipThrust)
+        oldThrust_array.append(oldLongThrust)
+
+        ###########################################################################################################################################################
+
+            
+    plotTraj(x_positions, y_positions, levercoordsplot, idealcoordsplot, error_values)
+    errors.clear()
+
+    plot_e_phi_Values(e_phi_values)
+
+    plot_e_l_Values(saturated_e_l_values)
+
+    plot_balance_Values(steering_inputs, refLine_orientations)
+
+    plot_Angles(yawAngles, refLine_orientations)
+
+    # plot_corners(refLine_orientations, refDistances, tVCs)
+
+    plot_Torque(MDriveRL_array, MDriveRR_array, MBrakeFL_array, MBrakeFR_array, MBrakeRL_array, MBrakeRR_array)
+
+    plot_TargetValues(tVelocity_array, acc_array, velocity_X)
+
+    # plot_longThrustValues(oldThrust_array, slipThrust_array)
+
+if __name__ == "__main__":
+    main()
---
+++ Benchmark_Model/Code/2 - Balance_Integrated_Tracks/Misano/steer_control_v3_4_Misano.py
@@ -0,0 +1,289 @@
+import numpy as np
+import math
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+import pandas as pd
+
+file_path = 'Misano_Traj_Mat.xlsx'
+trajectory_matrix = pd.read_excel(file_path)
+trajectory_matrix.head(), trajectory_matrix.columns
+
+
+sdistance = 0
+nLap = 0
+
+
+def refStateNow(trajectory_matrix, Vx, Vy, orientation, timestep):
+    # Calculates the current reference state of the vehicle given the trajectory T, velocity V, orientation, and a timestep.
+    # The code calculates the reference state, taking into account the vehicle’s orientation and updating the reference distance based on velocity and orientation
+    # Corresponds with the paper’s approach of continuously updating the vehicle's reference position along the trajectory
+
+    global sdistance
+    global nLap
+
+    max_sdistance = np.max(trajectory_matrix['sDistance'])
+
+    V_magnitude = np.sqrt(Vx**2 + Vy**2)
+    
+    # Create an interpolation function based on trajectory data
+    phi_interpolator = interp1d(trajectory_matrix['sDistance'], trajectory_matrix['Orientation2'], kind='linear', fill_value="extrapolate")
+    # print(f"sLap: ", trajectory_matrix [:, 0])
+    phi = phi_interpolator(sdistance)
+    # phi = phi - np.pi
+    # phi = phi + np.pi
+
+    sdistance += V_magnitude * timestep  * max(0, np.cos(orientation - phi))
+    # sdistance += V_magnitude * timestep * np.cos(orientation - phi)
+
+    if sdistance >= max_sdistance:
+        sdistance -= max_sdistance
+        phi = phi_interpolator(sdistance)
+
+        # # Determine if we are going clockwise or anticlockwise
+        # if trajectory_matrix[0, 2] > trajectory_matrix[-1, 2]:
+        #     # Anticlockwise
+        #     nLap -= 1
+        # else:
+        #     # Clockwise
+        #     nLap += 1
+
+    # print(f"sdistance: " , sdistance)
+
+    refDistance = sdistance
+    # print(f"Reference Orientation Make Up: " , phi, nLap)
+    refOrientation = phi + nLap * 2 * np.pi
+
+    # print(f"Reference Distance: " , refDistance)
+    # print(f"Reference Orientation: " , refOrientation)
+
+    return refDistance, refOrientation
+
+
+def LeverDistances(longitudinal_velocity):
+    # Returns a list of distances from a base point, scaled by the long. velocity component.
+    # Creates an array of distances based on the longitudinal velocity, which represent the preview points mentioned in the paper
+
+    longitudinal_velocity = abs(longitudinal_velocity)
+    L = np.zeros(8)
+
+    L[0] = 0
+    L[1] = longitudinal_velocity * 0.1
+    L[2] = longitudinal_velocity * 0.2
+    L[3] = longitudinal_velocity * 0.3
+    L[4] = longitudinal_velocity * 0.4
+    L[5] = longitudinal_velocity * 0.6
+    L[6] = longitudinal_velocity * 0.8
+    L[7] = longitudinal_velocity * 1.0 # Equation 4.20
+
+    return L
+
+
+def CorrespondingIdealPathPoints(trajectory_matrix, lever_distances, refDistance):
+    # Computes points on the ideal path based on lever distances and the reference distance.
+    # Uses interpolation to find corresponding points on the trajectory.
+    ideal_coordinates = []
+
+    # Initialize the result matrix r with zeros
+    r = np.zeros((8, 2))
+    
+    # Extract the maximum distance from the trajectory data
+    max_sdistance = np.max(trajectory_matrix['sDistance'])
+    # print("Max Distance: ", max_sdistance)
+    
+    # Create interpolation functions for the x and y coordinates
+    xr_interpolator = interp1d(trajectory_matrix['sDistance'], trajectory_matrix['x'], kind='linear', fill_value="extrapolate")
+    yr_interpolator = interp1d(trajectory_matrix['sDistance'], trajectory_matrix['y'], kind='linear', fill_value="extrapolate")
+    
+    for i in range(8):
+        targetDistance = refDistance + lever_distances[i] # Equation 4.22
+        # print("Target Distance: ", targetDistance)
+        if targetDistance > max_sdistance:
+            targetDistance -= max_sdistance
+        
+        # Interpolate the x and y coordinates at the target distance
+        xr = xr_interpolator(targetDistance)
+        yr = yr_interpolator(targetDistance) # Equation 4.23
+        r[i, :] = [xr, yr]
+
+    # print(f"Ideal path points: " , r)
+    ideal_coordinates.append(r)
+
+    return r, ideal_coordinates
+
+
+def LeverAbsoluteCoordinates(lever_distances, start_x, start_y, orientation):
+    # Computes absolute coordinates of points relative to a base position (start_x, start_y) using lever distances and orientation
+
+    lever_coordinates = []
+
+    # Initialize the result array with zeros
+    l = np.zeros((8, 2))
+    
+    for i in range(8):
+        # Calculate the coordinates based on the orientation and lever distances
+        xL = start_x + lever_distances[i] * math.cos(orientation)
+        yL = start_y + lever_distances[i] * math.sin(orientation) # Equation 4.21
+        # xL = start_x + lever_distances[i]
+        # yL = start_y + lever_distances[i]
+        l[i, :] = [xL, yL]
+
+    # print(f"Lever Coordinates: " , l)
+    lever_coordinates.append(l)
+    # print(l)
+
+    return l, lever_coordinates
+
+
+def AngularOffset(trajectory_matrix, refDistance, orientation, e_phi_gain):
+    # Computes the angular offset from the ideal orientation by comparing the current orientation with the reference orientation (trajectory orientation)
+
+    refLine_orientation = []
+    phi_interpolator = interp1d(trajectory_matrix['sDistance'], trajectory_matrix['Orientation2'], kind='linear', fill_value="extrapolate") # Equation 4.25
+    phi = phi_interpolator(refDistance)
+    # print("phi: ", phi)
+    # print("Reference Distance: ", refDistance)
+    # phi = phi - np.pi
+    refLine_orientation.append(phi)
+
+    # Calculate the angular offset
+    e_phi = phi - orientation # Equation 4.26
+    # e_phi = orientation - phi  # Equation 4.26
+
+    e_phi *= e_phi_gain
+
+    # print(e_phi)
+
+
+
+    return e_phi, refLine_orientation
+
+
+def LateralOffset(r, l, orientation, gains, lower_limits, upper_limits):
+    # Calculates the lateral offsets between ideal path points and actual lever points based on orientation
+
+    # Initialize the result array for lateral offsets
+    # orientation = -orientation
+    e_l = np.zeros(8)
+    saturated_e_l = []
+    error_values_gains_array = []
+
+    for i in range(8):
+        # Calculate the lateral offset based on the orientation
+        offset = (r[i, 1] - l[i, 1]) * math.cos(orientation) - (r[i, 0] - l[i, 0]) * math.sin(orientation) # Equation 4.24
+        # print(offset)
+
+        e_l[i] = offset * gains[i]
+        # print(e_l[i])
+        # error_values_gains_array.append(e_l)
+
+    
+
+    saturated_e_l = np.array([saturation(e_l[i], lower_limits[i], upper_limits[i]) for i in range(8)])
+    # print(saturated_e_l)
+
+    # saturated_e_l.append(saturated_e_l)
+
+    total_e_l = np.sum(saturated_e_l)
+    # print(total_e_l)
+    saturated_total_e_l = saturation(total_e_l, -18, 18)
+    # print(saturated_total_e_l)
+    # saturated_total_e_l = saturation(total_e_l, -10, 10)
+
+    return e_l, saturated_e_l, saturated_total_e_l, error_values_gains_array
+
+
+def saturation(value, min_value, max_value):
+    # Apply saturation limits to an array of values.
+
+    saturated_values = np.clip(value, min_value, max_value)
+    return saturated_values
+
+def steering_control_output(e_phi, saturated_total_e_l):
+    unsaturated_steering_controls = e_phi + saturated_total_e_l
+    steering_control_input = saturation(unsaturated_steering_controls, -28, 28)
+    steering_control_input = -steering_control_input
+    return steering_control_input
+
+
+
+##################################################################################################################
+# MAIN FUNCTION
+
+
+
+def calculate_steering_control(Vx, Vy, yawAngle, x, y):
+
+
+    # Initial parameters
+    timestep = 0.002  # Time step
+
+    # Calculate Lever Distances
+    lever_distances = LeverDistances(Vx)
+
+    steering_outputs = []
+    
+
+
+    current_position = x, y
+
+
+    # Reference State Now
+    ref_distance, ref_orientation = refStateNow(trajectory_matrix, Vx, Vy, yawAngle, timestep)
+
+    # Corresponding Ideal Path Points
+    ideal_path_points, ideal_coordinates_plot = CorrespondingIdealPathPoints(trajectory_matrix, lever_distances, ref_distance)
+
+    # Lever Absolute Coordinates
+    lever_absolute_coordinates, lever_coords_plot = LeverAbsoluteCoordinates(lever_distances, current_position[0], current_position[1], yawAngle)
+
+    # Angular Offset
+    e_phi_gain = 2
+    # e_phi_gain = 0.5235987756
+    e_phi, refLine_orientation = AngularOffset(trajectory_matrix, ref_distance, yawAngle, e_phi_gain)
+    # e_phi = AngularOffset(current_orientation, ref_orientation, e_phi_gain)
+
+    # Angular Offset Test
+    # e_phi_gain = 0.5
+    # e_phi = AngularOffset(current_orientation, ref_orientation, e_phi_gain)
+
+    # Matlab Values
+    e_l_gains = np.array([3, 2, 1.3, 0.8, 0.55, 0.32, 0.12, 0.04])
+    e_l_upper_limits = np.array([1.5, 1.5, 2, 2.75, 3, 2.75, 2.25, 1.7])
+    e_l_lower_limits = np.array([-1.5, -1.5, -2, -2.75, -3, -2.75, -2.25, -1.7])
+
+    # Test Values
+    # e_l_gains = np.array([0.5,   0.25,  0.15,  0.1,   0.075, 0.05,  0.025, 0.01])
+    # e_l_upper_limits = np.array([0.75,  0.75,  1,    1.375, 1.5,   1.375, 1.125, 0.85])
+    # e_l_lower_limits = np.array([-0.75,  -0.75,  -1,    -1.375, -1.5,   -1.375, -1.125, -0.85])
+
+    # Paper Values
+    # e_l_gains = np.array([10, 10, 6, 2, 0.8, 0.16, 0.04, 0.01])
+    # e_l_upper_limits = np.array([1, 2, 2, 2, 2, 1, 1, 1])
+    # e_l_lower_limits = np.array([-1, -2, -2, -2, -2, -1, -1, -1])
+
+    # Lateral Offset
+    e_l, saturated_e_l, total_saturated_e_l, error_values_gains_array = LateralOffset(ideal_path_points, lever_absolute_coordinates, yawAngle, e_l_gains, e_l_lower_limits, e_l_upper_limits)
+
+    # print("Angular Offset (e_phi):", e_phi)
+    # print("Lateral Offsets (e_l):", e_l)
+    # print("Total Saturated Lateral Offset (total_saturated_e_l):", total_saturated_e_l)
+
+    # Steering Control
+    steering_control_input = steering_control_output(e_phi, total_saturated_e_l)
+    steering_outputs.append(steering_control_input)
+
+
+
+    # print("Steering Control:", steering_control_input)
+    
+    return steering_control_input, e_phi, saturated_e_l, lever_coords_plot, ideal_coordinates_plot, e_l, refLine_orientation
+
+
+
+
+
+
+if __name__ == "__main__":
+    calculate_steering_control()
+    
